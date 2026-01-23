6d2a7ed339dd48d8fc4e2ecaac4793f631af561a
Connor Mallinger <cmallinger26@students.polytechnic.org>
2026-01-23 11:44:46 -0800

    Field for Song

--- Song.java
+++ Song.java
@@ -5,7 +5,10 @@
  */
 public class Song {
     //Fields-- what information do we want each Song to store?
-
+    private String title;
+    private String artist;
+    private int minute;
+    private int seconds;
 
 
 
